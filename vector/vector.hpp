661a10701083cad81fe1c158f5850f9756ce7600
liyuu1ove <azurezhao@foxmail.com>
2024-11-21 15:23:29 +0800

    test

---
+++ vector/vector.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+class Vertex 
+{
+private:
+    int m_x,m_y,m_z;
+public:
+    static int counter_created;
+    static int counter_copied;
+    Vertex(int x,int y,int z)
+    :m_x(x),m_y(y),m_z(z){
+        ++counter_created;
+        std::cout << "Created!" << Vertex::counter_created << std::endl ;
+    }
+    Vertex(const Vertex& vertex){
+        ++counter_copied;
+        std::cout << "Copied!" << Vertex::counter_copied << std::endl ;
+    }
+    void operator<< (const Vertex&){
+        std::cout << this->m_x << '.' << this->m_y << '.' << this->m_z << std::endl;
+    }
+};
